2038ef349b8cca0d22e7cbfb1158230b8d7e42f9
Sarah <92927637+SarahGrozav1@users.noreply.github.com>
2023-12-20 15:09:20 +0000

    Setup views and urls and added index.html file

--- jewelleryprj/core/models.py
+++ jewelleryprj/core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#  Category model
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Product model
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Order model
+
+
+
+
+
+
+
+# Customer model
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
--- jewelleryprj/core/views.py
+++ jewelleryprj/core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("welcome to my shop")
+    return render(request, 'core/index.html')
