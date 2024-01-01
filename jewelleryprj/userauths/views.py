5f75002b5c96f9ea1870458bea36a1ba687cd187
Sarah <92927637+SarahGrozav1@users.noreply.github.com>
2024-01-01 06:51:14 +0000

    Added link to the login page ,work in progress

--- jewelleryprj/userauths/views.py
+++ jewelleryprj/userauths/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 # from userauths.forms import UserRegisterForm
 # from django.contrib.auth import login, authenticate
-# from django.contrib import messages
+from django.contrib import messages
 # from django.shortcuts import redirect
 
 # from django.contrib.auth.views import LoginView
@@ -24,9 +24,11 @@ def perform_login(request):
         user_obj = authenticate(request, username=username, email=email, password1=password1, password2=password2)
         if user_obj is not None:
             login(request, user_obj)
+            messages.success(request, f'Hey {username}, your account was created successfully!')
             return HttpResponseRedirect(reverse("core/dashboard"))
         else:
             return render(request, "userauths/sign-up.html/")
+            
         
 def dashboard(request):
     return render(request, "core/dashboard.html")
@@ -45,7 +47,7 @@ def perform_logout(request):
 #         if form.is_valid():
 #             new_user = form.save()
 #             username = form.cleaned_data.get("username")
-#             messages.success(request, f'Hey {username}, your account was created successfully!')
+            # messages.success(request, f'Hey {username}, your account was created successfully!')
 #             new_user = authenticate(username=form.cleaned_data['email'], 
 #                                     password=form.cleaned_data['password1']
 #             )
